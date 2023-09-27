28e4b6ba9e3ccd0b6b74226852dddff0420f4546
Matheus Machado <143560811+mateco@users.noreply.github.com>
2023-09-26 21:24:08 -0300

    Add files via upload

---
+++ vs testes/script.js
@@ -0,0 +1,8 @@
+function changeBackgroundColor(color) {
+    console.log(color)    
+    const selectedColor = color;
+    document.body.style.backgroundColor = selectedColor;
+}
+function showcolors(){
+    document.getElementById('change').style.opacity == 0 ?  document.getElementById('change').style.opacity = 1 : document.getElementById('change').style.opacity = 0;
+}
